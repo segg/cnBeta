5195596ab2925ca3b11a3349d7dcc6f3c8ce5a4a
segg <wzg312@msn.com>
2013-06-30 18:35:42 -0700

    use dedicated ip address

--- cnBeta/src/gg/cnbeta/activity/NewsListActivity.java
+++ cnBeta/src/gg/cnbeta/activity/NewsListActivity.java
@@ -35,9 +35,9 @@ import com.markupartist.android.widget.ActionBar.AbstractAction;
 public class NewsListActivity extends Activity {
 	
 	public static final String URL_APPSPOT = "http://gg--uu.appspot.com";
-	public static final String URL_PROXY = "http://kaituan.org/cgi-bin/nph-x.cgi/w0/http/gg--uu.appspot.com";
+	public static final String URL_PROXY = "http://50.87.186.28/cgi-bin/cnbeta_newslist.sh";
 	public static final String URL_GAE_NEWS_LIST = URL_APPSPOT + "/feed?firstArticleId=";
-	public static final String URL_PROXY_NEWS_LIST = URL_PROXY + "/feed?firstArticleId=";
+	public static final String URL_PROXY_NEWS_LIST = URL_PROXY + "?";
 	public static final String URL_GAE_NEWS_CONTENT = URL_APPSPOT + "/feed?id=";
 	public static final String ENCODING_DEFAULT = "UTF-8";
 	public static final String URL_CNBETA = "http://cnbeta.com";
